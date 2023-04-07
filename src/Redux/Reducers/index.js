e3a6d5e47bdfb0fa4f72ccf7d3cfd51dba3f0c80
VittoriaBasile <vichy.basile@gmail.com>
2023-04-07 11:02:46 +0200

    first commit

---
+++ src/Redux/Reducers/index.js
@@ -0,0 +1,9 @@
+const InitialState = {};
+const mainReducer = (state = InitialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default mainReducer;
